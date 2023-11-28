e361b7b4b0f2e1fefb3c2ca257b31b7f4dadd6c1
braidonw <braidon@braidonwhatley.com>
2023-11-28 22:33:02 +1100

    ready to run

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,19 @@
 use log::info;
-use protohackers_rs::{prime_time, smoke_test};
+use protohackers_rs::{means_to_an_end, prime_time, smoke_test};
 use tokio::join;
 
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
     env_logger::init();
-    log!("Running Protohackers Servers");
+    info!("Running Protohackers Servers");
 
     let _ = join!(
-        tokio::spawn(async move { prime_time::run("10001").await.unwrap() }),
         tokio::spawn(async move {
             smoke_test::run("10000").await.unwrap();
+        }),
+        tokio::spawn(async move { prime_time::run("10001").await.unwrap() }),
+        tokio::spawn(async move {
+            means_to_an_end::run("10002").await.unwrap();
         })
     );
 
